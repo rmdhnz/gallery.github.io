842620c833f9619f581d9452a167a24f8e0ffc8e
rmdhnz <ramadhanfadhly2@gmail.com>
2022-08-04 16:35:10 +0700

    membuat image gallery

---
+++ script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.container .jumbo');
+const thumbs = document.querySelectorAll('.thumb');
+container.addEventListener('click',function(e){
+    if(e.target.className=='thumb'){
+        jumbo.style.backgroundImage = `url(${e.target.src})`;
+        jumbo.classList.add('fade');
+        setTimeout(function(){
+            jumbo.classList.remove('fade');
+        },500);
+        thumbs.forEach(function(thumb){
+            thumb.className= 'thumb';
+        });
+        e.target.classList.add('active');
+    }
+});
+const button = document.createElement('button');
+button.setAttribute('type','button');
+button.appendChild(document.createTextNode('reset'));
+container.after(button);
+button.style.padding = "10px";
+button.style.cursor = "pointer";
+
+button.addEventListener('click',function(){
+    jumbo.removeAttribute('style');
+    thumbs.forEach(function(thumb){
+        thumb.className = 'thumb';
+    });
+});
